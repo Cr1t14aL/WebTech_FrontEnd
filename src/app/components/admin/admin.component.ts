96e4d7cd4c841a48191e8a47e9e5df359814c55c
Poompisek <poompisek.poom@g.swu.ac.th>
2017-12-15 20:47:54 +0700

    admin page

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import {Router} from '@angular/router';
+import { Router } from '@angular/router';
 import { User } from '../../Models/user.model';
 import { UserService } from '../../services/user.service';
 import { DetailsService } from '../../services/foodlist.service';
 import { AuthGuard } from '../../auth-guard.service'
+import { Details } from '../../Models/detail.model';
 
 @Component({
   selector: 'app-admin',
@@ -11,21 +12,39 @@ import { AuthGuard } from '../../auth-guard.service'
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent implements OnInit {
+  foodlist: Details;
+  err: string[] = [];
 
   private userList: User[];
+  private detailList : Details[];  
+
 
   constructor(
-    private router:Router, 
+    private router: Router,
     private userService: UserService,
-    private auth: AuthGuard
+    private auth: AuthGuard,
+    private detailsService:DetailsService,
   ) { }
 
   ngOnInit() {
 
-      this.userService.getUsers().subscribe((response) => {
-        this.userList = response;
-      })   
+    this.userService.getUsers().subscribe((response) => {
+      this.userList = response;
+    })
 
+    this.detailsService.getDetail().subscribe((response) => { 
+      this.detailList = response;
+    })
+
+    // this.foodlist = new Details();
+  }
+  // Anout User
+  deleteUser(){
+    
   }
 
+  // About FoodList
+  addFoodList(){    
+
+  }
 }
